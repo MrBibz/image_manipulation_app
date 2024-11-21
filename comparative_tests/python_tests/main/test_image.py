5f906f933e7a6edc66440cb79953721a0480f0d0
Nicolas Bibeau <mrnicolasbibeau@gmail.com>
2024-11-20 23:36:10 -0500

    Fix test_image.py to only display image without having to choose it

--- comparative_tests/python_tests/main/test_image.py
+++ comparative_tests/python_tests/main/test_image.py
@@ -1,8 +1,8 @@
 import tkinter as tk
-from tkinter import filedialog, Canvas
+from tkinter import Canvas
 from PIL import Image, ImageTk
 
-# Modes de mise à l'échelle en tant que constantes
+# Scaling modes as constants
 CONTAIN = 1
 COVER = 2
 SCALE_DOWN = 3
@@ -17,16 +17,11 @@ class ImageWidget:
 
         self.image = None
         self.tk_image = None
-        self.fit = CONTAIN  # Mode de mise à l'échelle par défaut
+        self.fit = CONTAIN  # Default scaling mode
         self.scale = 1.0
 
-        # Menu
+        # Menu (only for changing the scaling mode)
         menubar = tk.Menu(root)
-        file_menu = tk.Menu(menubar, tearoff=0)
-        file_menu.add_command(label="Open Image", command=self.open_image)
-        file_menu.add_command(label="Exit", command=root.quit)
-        menubar.add_cascade(label="File", menu=file_menu)
-
         fit_menu = tk.Menu(menubar, tearoff=0)
         fit_menu.add_command(label="Contain", command=lambda: self.set_fit(CONTAIN))
         fit_menu.add_command(label="Cover", command=lambda: self.set_fit(COVER))
@@ -37,14 +32,16 @@ class ImageWidget:
         root.config(menu=menubar)
         root.bind("<Configure>", self.resize_image)
 
-    def open_image(self):
-        file_path = filedialog.askopenfilename(
-            title="Open Image",
-            filetypes=[("Image Files", "*.jpg;*.jpeg;*.png;*.bmp;*.gif")]
-        )
-        if file_path:
-            self.image = Image.open(file_path)
+        # Load a default image
+        self.load_image("../../images/bread.jpg")
+
+    def load_image(self, image_path):
+        """ Load the specified image and resize it """
+        try:
+            self.image = Image.open(image_path)
             self.resize_image()
+        except FileNotFoundError:
+            print(f"Image not found: {image_path}")
 
     def set_fit(self, fit_mode):
         self.fit = fit_mode
@@ -79,7 +76,7 @@ class ImageWidget:
         else:
             new_width, new_height = int(img_width * scale), int(img_height * scale)
 
-        resized_image = self.image.resize((new_width, new_height), Image.ANTIALIAS)
+        resized_image = self.image.resize((new_width, new_height), Image.Resampling.LANCZOS)
         self.tk_image = ImageTk.PhotoImage(resized_image)
         self.canvas.create_image(canvas_width // 2, canvas_height // 2, image=self.tk_image, anchor=tk.CENTER)
 
